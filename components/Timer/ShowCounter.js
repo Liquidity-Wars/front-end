e352375a6e861d3e02337a4367095a2eeb90aa67
yichulau <yichu21@gmail.com>
2022-11-01 08:54:57 +0800

    frontend changes

--- components/Timer/ShowCounter.js
+++ components/Timer/ShowCounter.js
@@ -4,11 +4,11 @@ import DateTimeDisplay from './DateTimeDisplay';
 const ShowCounter = ({ days, hours, minutes, seconds }) => {
   return (
     <>
-        <div className="flex flex-row">
+        <div className="flex flex-row items-center">
             <DateTimeDisplay value={hours} type={'Hours'} isDanger={false} />
-            <p>:</p>
+            <img src='../assets/images/horseEMOJI.png' className="w-4 h-4" alt="horse"/>
             <DateTimeDisplay value={minutes} type={'Mins'} isDanger={false} />
-            <p>:</p>
+            <img src='../assets/images/horseEMOJI.png' className="w-4 h-4" alt="horse"/>
             <DateTimeDisplay value={seconds} type={'Seconds'} isDanger={false} />
         </div>
     </>
