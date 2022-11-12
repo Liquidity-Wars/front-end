8484e25abad32b78b71357390721f4133ee05b6c
yichulau <yichu21@gmail.com>
2022-11-12 15:42:22 +0800

    code changses

--- components/Misc/Selector.js
+++ components/Misc/Selector.js
@@ -3,18 +3,11 @@ import { createPopper } from "@popperjs/core";
 
 const Selector = () => {
 
-    const [countries, setCountries] = useState(null);
-    const [inputValue, setInputValue] = useState("");
+    // const [countries, setCountries] = useState(null);
+    // const [inputValue, setInputValue] = useState("");
     const [selected, setSelected] = useState("");
     const [open, setOpen] = useState(false);
   
-    useEffect(() => {
-      fetch("https://restcountries.com/v2/all?fields=name")
-        .then((res) => res.json())
-        .then((data) => {
-          setCountries(data);
-        });
-    }, []);
 
 
   return (
@@ -38,42 +31,10 @@ const Selector = () => {
           open ? "max-h-60" : "max-h-0"
         } `}
       >
-        {/* <div className="flex items-center px-2 sticky top-0 bg-white">
-          <input
-            type="text"
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value.toLowerCase())}
-            placeholder="Enter country name"
-            className="placeholder:text-gray-700 p-2 outline-none"
-          />
-        </div> */}
         <li className='p-2 text-sm hover:bg-sky-600 hover:text-white'>
             Token1
         </li>
-        {/* {countries?.map((country) => (
-          <li
-            key={country?.name}
-            className={`p-2 text-sm hover:bg-sky-600 hover:text-white
-            ${
-              country?.name?.toLowerCase() === selected?.toLowerCase() &&
-              "bg-sky-600 text-white"
-            }
-            ${
-              country?.name?.toLowerCase().startsWith(inputValue)
-                ? "block"
-                : "hidden"
-            }`}
-            onClick={() => {
-              if (country?.name?.toLowerCase() !== selected.toLowerCase()) {
-                setSelected(country?.name);
-                setOpen(false);
-                setInputValue("");
-              }
-            }}
-          >
-            {country?.name}
-          </li>
-        ))} */}
+
       </ul>
     </div>
     </>
