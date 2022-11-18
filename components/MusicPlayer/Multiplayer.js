01d8f2b2e7fb602290ed60359bcbf44071c832ee
yichulau <yichu21@gmail.com>
2022-11-18 15:11:34 +0800

    multiplayer

--- components/MusicPlayer/Multiplayer.js
+++ components/MusicPlayer/Multiplayer.js
@@ -52,17 +52,8 @@ const Multiplayer = () => {
               }}
               whileTap={{ scale: 0.9 }}
               onClick={() => handleClick()}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#AB4A05" className="w-8 h-8">
-                  <motion.path 
-                    initial={{
-                      rotate: -45,
-                      pathLength: 0, 
-                    }}
-                    animate={{
-                      rotate: 0,
-                      pathLength: 1, 
-                    }}
-                  strokeLinecap="round" strokeLinejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke={`${played !== true ? "#AB4A05" : "#CF3810"}`} className="w-8 h-8">
+                  <motion.path strokeLinecap="round" strokeLinejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                   <path strokeLinecap="round" strokeLinejoin="round" d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z" />
                 </svg>
             </motion.button>
@@ -77,8 +68,30 @@ const Multiplayer = () => {
               strokeLinecap="round" strokeLinejoin="round" d="M3 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062A1.125 1.125 0 013 16.81V8.688zM12.75 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062a1.125 1.125 0 01-1.683-.977V8.688z" />
             </svg>
         </div>
-        <div className='flex'>
-          <h2 className="text-lg text-[#CF3810] font-['Nabana-bold'] font-bold">Playing ~Valley Mix~</h2>
+        <div className='flex items-center'>
+          <h2 className="text-lg text-[#CF3810] font-['Nabana-bold'] font-bold">{played === true ? "Playing ~Valley Mix~" : "Press Play To Start!" }</h2>
+          {played === true ? (
+            <motion.svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#AB4A05" className="w-4 h-4 mx-1">
+              <motion.path animate={{
+                scale: [0.5, 1, 0.5],
+                rotate: [0, 0, 0, 0, 0],
+                borderRadius: ["0%", "50%", "50%"]
+                }}
+                transition={{
+                  duration: 2,
+                  ease: "easeInOut",
+                  times: [0, 0.5, 1],
+                  repeat: Infinity,
+                  repeatDelay: 0.1
+                }} 
+                strokeLinecap="round" strokeLinejoin="round" d="M19.114 5.636a9 9 0 010 12.728M16.463 8.288a5.25 5.25 0 010 7.424M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z" />
+            </motion.svg>
+          ) : (
+            <motion.svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#AB4A05" className="w-4 h-4 mx-1">
+              <motion.path strokeLinecap="round" strokeLinejoin="round" d="M19.114 5.636a9 9 0 010 12.728M16.463 8.288a5.25 5.25 0 010 7.424M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z" />
+            </motion.svg>
+          )}
+ 
         </div>
       </div>
 
